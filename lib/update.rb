5e6b7dc9675e60f82df10dba689bf7eb3a99083b
Shannon <shannonskipper@gmail.com>
2012-01-24 16:04:14 -0800

    refactored command into instance variable

--- lib/update.rb
+++ lib/update.rb
@@ -15,19 +15,20 @@ module Update
     
     def run_commands
       Update::COMMANDS.each do |command, description|
+        @command = command
         green description
         run_command_and_print_output
-        check_for_failures command
+        check_for_failures
       end
     end
     
     def run_command_and_print_output
-      IO.popen(command) do |io|
-        puts io.readline
+      IO.popen @command do |io|
+        puts io.read
       end
     end
     
-    def check_for_failures command
+    def check_for_failures
       unless $?.success?
         @failed ||= []
         @failed << command
