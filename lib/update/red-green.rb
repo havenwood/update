cd44f4e112afc77d35ce28b49fc34156082d52ca
Shannon <shannonskipper@gmail.com>
2012-01-29 18:59:40 -0800

    trivial refactor

--- lib/update/red-green.rb
+++ lib/update/red-green.rb
@@ -1,6 +1,5 @@
 module RedGreen
-  RED, GREEN = "tput setaf 1", "tput setaf 2"
-  RESET = "tput sgr0"
+  RED, GREEN, RESET = "tput setaf 1", "tput setaf 2", "tput sgr0"
   
   def green message
     system GREEN
