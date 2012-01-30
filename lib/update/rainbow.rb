98b705b2d358e50e5316b21b5ba395246988eb01
Shannon <shannonskipper@gmail.com>
2012-01-29 20:24:45 -0800

    rainbows are to colors as unicorns are to zebras?

---
+++ lib/update/rainbow.rb
@@ -0,0 +1,17 @@
+module Rainbow
+  RESET = "tput sgr0"
+  
+  def rainbow_puts color, message
+    system color
+    puts message
+    system RESET
+  end
+
+  def green message
+    rainbow_puts "tput setaf 2", message
+  end
+  
+  def red message
+    rainbow_puts "tput setaf 1", message
+  end
+end
