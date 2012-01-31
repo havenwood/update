7c98b37eda2f166565ec2949805924b76bed1f60
Shannon <shannonskipper@gmail.com>
2012-01-30 22:16:21 -0800

    refactored

--- lib/update/rainbow.rb
+++ lib/update/rainbow.rb
@@ -1,17 +1,21 @@
 module Rainbow
-  RESET = "tput sgr0"
-  
-  def rainbow_puts color, message
-    system color
-    puts message
-    system RESET
+  def default text
+    rainbow "tput sgr0", text
   end
 
-  def green message
-    rainbow_puts "tput setaf 2", message
+  def green text
+    rainbow "tput setaf 2", text
   end
   
-  def red message
-    rainbow_puts "tput setaf 1", message
+  def red text
+    rainbow "tput setaf 1", text
+  end
+  
+  private
+
+  def rainbow color, text
+    system color
+    puts text
+    system "tput sgr0" #set text color back to default
   end
 end
