2c4ecf3b70adb4f0ac974518c9349c9ecc5fc604
Shannon <shannonskipper@gmail.com>
2012-02-01 10:30:15 -0800

    modified commands to be single threaded

--- lib/update/commands.rb
+++ lib/update/commands.rb
@@ -1,14 +1,14 @@
 module Update
-  COMMANDS = [{
+  COMMANDS = {
     "brew update" => "Checking for updated Brew packages...",
     "brew upgrade" => "Installing updated Brew packages...",
-    "brew cleanup --force" => "Cleaning up outdated packages..." },{
+    "brew cleanup --force" => "Cleaning up outdated packages...",
       
     ##
     # Create additional command-groups by uncommenting the following-ish:
     
     # "ls -la" => "Look around...",
-    # "uname -a" => "Whatchamacallit..." },{
+    # "uname -a" => "Whatchamacallit...",
 
     "rvm get head --auto" => "Get the latest RVM...",
     "rvm 1.9.3-head do gem update --system" => "Update 1.9.3's RubyGems version...",
@@ -16,5 +16,5 @@ module Update
     "rvm 1.9.3-head do gem cleanup -d" => "Show 1.9.3 gem cleanup dry-run...",
     "rvm rbx-head do gem update --system" => "Update Rubinius' RubyGems version...",
     "rvm rbx-head do gem update" => "Update Rubinius gems...",
-    "rvm rbx-head do gem cleanup -d" => "Show Rubinius gem cleanup dry-run..." }]
+    "rvm rbx-head do gem cleanup -d" => "Show Rubinius gem cleanup dry-run..." }
 end
