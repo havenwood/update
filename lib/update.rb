daccd0e8b9839ed3669c353adda334b4e0378197
Shannon <shannonskipper@gmail.com>
2012-02-01 12:03:38 -0800

    simplified for single thread

--- lib/update.rb
+++ lib/update.rb
@@ -8,28 +8,22 @@ module Update
   class << self
     def run
       Update::COMMANDS.each do |commands|
-        @commands = commands
-        run_commands
+        commands.each do |command, description|
+          green description
+          puts `#{command}`
+          take_note_of_failures command
+        end
       end
       report_final_status
     end
     
     private
-    
-    def run_commands
-      @commands.each do |command, description|
-        @command = command
-        green description
-        puts `#{command}`
-        take_note_of_failures
-      end
-    end
 
-    def take_note_of_failures
+    def take_note_of_failures command
       unless $?.success?
         @failed ||= []
         @failed << @command
-        @failure_report = "Command failed: '#{@command}'"
+        @failure_report = "Command failed: '#{command}'"
         red @failure_report if @failure_report
       end
     end
