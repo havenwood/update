d868a2bf27ed4f83adea4b6c4fd0562e3c3a66cd
Shannon <shannonskipper@gmail.com>
2012-02-03 23:06:33 -0800

    renamed

---
+++ lib/update/red-green.rb
@@ -0,0 +1,17 @@
+module RedGreen
+  def red text
+    say "tput setaf 1", text
+  end
+  
+  def green text
+    say "tput setaf 2", text
+  end
+  
+  private
+
+  def say color, text
+    system color
+    puts text
+    system "tput sgr0" #default color
+  end
+end
