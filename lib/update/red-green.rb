b0bed5138354a2dd2039888e95cb8e1e9e87324c
Shannon Skipper <shannonskipper@gmail.com>
2012-02-22 18:15:39 -0800

    comments added for bashy stuff

--- lib/update/red-green.rb
+++ lib/update/red-green.rb
@@ -2,14 +2,14 @@ module RedGreen
 	private
 	
 	def red text
-		system "tput setaf 1"
+		system "tput setaf 1" #red text
 		STDERR.puts text
-		system "tput sgr0" #default color
+		system "tput sgr0" #default text
 	end
 	
 	def green text
-		system "tput setaf 2"
+		system "tput setaf 2" #green text
 		puts text
-		system "tput sgr0" #default color
+		system "tput sgr0" #default text
 	end
 end
