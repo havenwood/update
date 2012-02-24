2d6aaab65b901bd4acddd96fa9df3e4cab64ea0f
Shannon Skipper <shannonskipper@gmail.com>
2012-02-24 15:01:52 -0800

    convert indentation to spaces

--- lib/update/red-green.rb
+++ lib/update/red-green.rb
@@ -1,15 +1,15 @@
 module RedGreen
-	private
-	
-	def red text
-		system "tput setaf 1" #red text
-		STDERR.puts text
-		system "tput sgr0" #default text
-	end
-	
-	def green text
-		system "tput setaf 2" #green text
-		puts text
-		system "tput sgr0" #default text
-	end
+  private
+  
+  def red text
+    system "tput setaf 1" #red text
+    STDERR.puts text
+    system "tput sgr0" #default text
+  end
+  
+  def green text
+    system "tput setaf 2" #green text
+    puts text
+    system "tput sgr0" #default text
+  end
 end
