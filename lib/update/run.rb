236c3a8e5a3f17f1fdd6495ce757f1addbed22e3
Shannon Skipper <shannonskipper@gmail.com>
2012-02-24 14:29:18 -0800

    refactored run command out to lib/

---
+++ lib/update/run.rb
@@ -0,0 +1,35 @@
+module Update
+  extend RedGreen
+
+  class << self
+    def run
+      Update::COMMANDS.each do |command, description|
+        @command = command
+        green description
+        puts `#@command`
+        take_note_if_command_fails
+      end
+      report_final_status
+    end
+
+    private
+
+    def take_note_if_command_fails
+      unless $?.success?
+        @failed ||= []
+        @failed << @command
+        red "Command failed: '#@command'"
+      end
+    end
+    
+    def report_final_status
+      if @failed
+        red "Update process completed with failures.\a" #chirp
+        @failed.each { |this_failed| puts "Command failed: '#{this_failed}'" }
+        exit 1
+      else
+        green "Update process completed successfully."
+      end
+    end
+  end
+end
