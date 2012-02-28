3eeb0669fb985f7a90ca8e469973a0f76d4182ad
Shannon Skipper <shannonskipper@gmail.com>
2012-02-28 12:33:50 -0800

    whitespace adjustment

--- lib/update/commands.rb
+++ lib/update/commands.rb
@@ -6,11 +6,11 @@ module Update
       "brew cleanup --force" => "Cleaning up outdated packages..."
     },
 
-  ### Add new command groups by uncommenting a hash such as below:
-  # {
-  #   "rvm get head --auto"  => "Getting the latest RVM...",
-  #   "rvm cleanup all"      => "Cleaning up everything RVM..."
-  # },
+  	### Add new command groups by uncommenting a hash such as below:
+	  # {
+	  #   "rvm get head --auto"  => "Getting the latest RVM...",
+	  #   "rvm cleanup all"      => "Cleaning up everything RVM..."
+	  # },
     
     { 
       "gem update --system"  => "Getting the latest RubyGems...",
