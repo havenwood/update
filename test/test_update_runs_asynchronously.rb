04d30c336bc6595495516fde3291b1df8ad70465
Shannon Skipper <shannonskipper@gmail.com>
2012-02-28 12:17:54 -0800

    edited for more consise meaning

--- test/test_update_runs_asynchronously.rb
+++ test/test_update_runs_asynchronously.rb
@@ -2,6 +2,6 @@ require_relative 'helper'
 
 class TestUpdate < MiniTest::Unit::TestCase
   def test_async
-    assert "I know not how to test this, which makes me sad.", "Must seek enlightenment, for there is none here."
+    assert "I know not how to test this, which makes me sad.", "Must seek enlightenment elsewhere, for there is none here."
   end
 end
