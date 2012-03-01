1b761fc1acb18e0ba86b4b43b91eeca318dcddf2
Shannon Skipper <shannonskipper@gmail.com>
2012-03-01 13:01:12 -0800

    fixed bug

--- lib/update/run-updates.rb
+++ lib/update/run-updates.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 module Update
   extend RedGreen
 
@@ -14,27 +16,21 @@ module Update
 
     def asynchronously_iterate_over_command_groups
       EM::Synchrony::FiberIterator.new(Update::COMMAND_GROUPS).each do |commands|
-        @commands = commands
+        commands.each do |command, description|
+          @command, @description = command, description #PROBLEM
 
-        synchronously_run_commands_within_each_command_group
+          synchronously_run_commands_within_each_command_group
+
+          take_note_if_command_fails
+        end
+
+        display_results_of_command_group
       end
 
       EventMachine.stop
     end
 
     def synchronously_run_commands_within_each_command_group
-      @commands.each do |command, description|
-        @command, @description = command, description
-
-        run_commands_in_each_command_group
-
-        take_note_if_command_fails
-      end
-
-      display_results_of_command_group
-    end
-
-    def run_commands_in_each_command_group
       @results ||= {}
       @results["#{@description}"] = `#@command`
     end
